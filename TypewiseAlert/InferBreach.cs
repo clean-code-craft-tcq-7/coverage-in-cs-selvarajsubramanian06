17b69332b8f9a292328eb6a7a58fd190625288a4
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-01-10 23:13:23 +0530

    Create InferBreach.cs

---
+++ TypewiseAlert/InferBreach.cs
@@ -0,0 +1,15 @@
+using System;
+using static TypewiseAlert.TypeWiseEnums
+
+namespace TypewiseAlert
+{
+    public static BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+      if(value < lowerLimit) {
+        return BreachType.TOO_LOW;
+      }
+      if(value > upperLimit) {
+        return BreachType.TOO_HIGH;
+      }
+      return BreachType.NORMAL;
+    }
+}    
