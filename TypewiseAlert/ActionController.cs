dddf72f47374c68d2ee5b6f23a82b3997f6fb5d6
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-01-10 23:29:33 +0530

    Update ActionController.cs

--- TypewiseAlert/ActionController.cs
+++ TypewiseAlert/ActionController.cs
@@ -3,20 +3,10 @@ using static TypewiseAlert.TypeWiseEnums
 
 namespace TypewiseAlert
 {
-
-    public static void sendToEmail(BreachType breachType) {
-          string recepient = "a.b@c.com";
-          switch(breachType) {
-            case BreachType.TOO_LOW:
-              Console.WriteLine("To: {}\n", recepient);
-              Console.WriteLine("Hi, the temperature is too low\n");
-              break;
-            case BreachType.TOO_HIGH:
-              Console.WriteLine("To: {}\n", recepient);
-              Console.WriteLine("Hi, the temperature is too high\n");
-              break;
-            case BreachType.NORMAL:
-              break;
-          }
-     }
+    public class ActionController{
+        public static void sendToController(BreachType breachType) {
+          const ushort header = 0xfeed;
+          Console.WriteLine("{} : {}\n", header, breachType);
+        }
+    }
 }     
