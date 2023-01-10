09bd9f863e8613283c9e9dd34a28761e43906422
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-01-10 23:17:11 +0530

    Create ClassifyTempBreach.cs

---
+++ TypewiseAlert/ClassifyTempBreach.cs
@@ -0,0 +1,28 @@
+using System;
+using static TypewiseAlert.TypeWiseEnums
+
+namespace TypewiseAlert
+{
+  public class ClassifyTempBreach{
+    public static BreachType classifyTemperatureBreach(
+          CoolingType coolingType, double temperatureInC) {
+        int lowerLimit = 0;
+        int upperLimit = 0;
+        switch(coolingType) {
+          case CoolingType.PASSIVE_COOLING:
+            lowerLimit = 0;
+            upperLimit = 35;
+            break;
+          case CoolingType.HI_ACTIVE_COOLING:
+            lowerLimit = 0;
+            upperLimit = 45;
+            break;
+          case CoolingType.MED_ACTIVE_COOLING:
+            lowerLimit = 0;
+            upperLimit = 40;
+            break;
+        }
+        return inferBreach(temperatureInC, lowerLimit, upperLimit);
+      }
+    }
+}    
