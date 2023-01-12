8acf930b11f8d1ea2b1552893640ba2374a7f4ca
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-01-12 14:00:37 +0530

    Update ActionEmail.cs

--- TypewiseAlert/ActionEmail.cs
+++ TypewiseAlert/ActionEmail.cs
@@ -5,17 +5,26 @@ namespace TypewiseAlert
 {
     public class ActionEmail
     {
-        public static void sendToEmail(BreachType breachType) {
+        public static bool sendToEmail(BreachType breachType) 
+        {
+        try 
+            {
+            List<EmailParam> listOfItems = new List<EmailParam>();
+            listOfItems.Add(new EmailParam() { Breach = BreachType.TOO_LOW, Recipient = "ab@c.com", EmailMessage = "Hi, the temperature is too low" });
+            listOfItems.Add(new EmailParam() { Breach = BreachType.TOO_HIGH, Recipient = "ab@d.com", EmailMessage = "Hi, the temperature is too high" });
+            listOfItems.Add(new EmailParam() { Breach = BreachType.NORMAL, Recipient = "ab@e.com", EmailMessage = "Hi, the temperature is normal" });
 
-        List<EmailParam> listOfItems = new List<EmailParam>();
-        listOfItems.Add(new EmailParam() { Breach = BreachType.TOO_LOW, Recipient = "ab@c.com", EmailMessage = "Hi, the temperature is too low" });
-        listOfItems.Add(new EmailParam() { Breach = BreachType.TOO_HIGH, Recipient = "ab@d.com", EmailMessage = "Hi, the temperature is too high" });
-        listOfItems.Add(new EmailParam() { Breach = BreachType.NORMAL, Recipient = "ab@e.com", EmailMessage = "Hi, the temperature is normal" });
+            EmailParam value = listOfItems.Find(m => m.Breach == breachType);
 
-        EmailParam value = listOfItems.Find(m => m.Breach == breachType);
-
-        Console.WriteLine(value.Recipient);
-        Console.WriteLine(value.EmailMessage);
-      }
+            Console.WriteLine(value.Recipient);
+            Console.WriteLine(value.EmailMessage);
+            
+            return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
     }
 }    
