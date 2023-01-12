26ac626247f70f5b11ef134ed45f0f6b8fe3874d
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-01-12 22:38:19 +0530

    Update Constants.cs

--- TypewiseAlert/Constants.cs
+++ TypewiseAlert/Constants.cs
@@ -6,7 +6,14 @@ namespace TypewiseAlert
     {
       public const string MailRecipient = "ab@c.com";
       public const string EmailMessage_Low = "Hi, the temperature is too low"; 
+      public const string EmailMessage_High = "Hi, the temperature is too high"; 
+      public const string EmailMessage_Normal = "Hi, the temperature is normal"; 
         
       public const double PassiveLowLimit = 0;
+      public const double PassiveHighLimit = 35;
+      public const double HiActiveLowLimit = 0;
+      public const double HiActiveHighLimit = 45;
+      public const double MedActiveLowLimit = 0;
+      public const double MedActiveHighLimit = 40;        
     }
 }
