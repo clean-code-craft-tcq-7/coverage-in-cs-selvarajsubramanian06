3c79f5ceb21c4a89f74d15061590d25ba7995ce1
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-01-12 22:07:38 +0530

    Update ActionController.cs

--- TypewiseAlert/ActionController.cs
+++ TypewiseAlert/ActionController.cs
@@ -6,10 +6,17 @@ namespace TypewiseAlert
     public class ActionController{
         public static bool sendToController(BreachType breachType) 
         {
+          try
+            {
                 const ushort header = 0xfeed;
                 Console.WriteLine("{} : {}\n", header, breachType);
 
                 return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
         }
     }
 }     
