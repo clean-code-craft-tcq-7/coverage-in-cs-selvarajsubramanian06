ab2f0d9b08727355cf979331488e97b035e6f581
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-01-12 16:18:17 +0530

    Create CoolingTypeM.cs

---
+++ TypewiseAlert/CoolingTypeM.cs
@@ -0,0 +1,12 @@
+using System;
+using static TypewiseAlert.TypeWiseEnums;
+
+namespace TypewiseAlert
+{
+    public class CoolingTypeM
+    {
+        public CoolingType Cooltype;
+        public double LowerLimit;
+        public double Upperlimit;
+    }
+}
