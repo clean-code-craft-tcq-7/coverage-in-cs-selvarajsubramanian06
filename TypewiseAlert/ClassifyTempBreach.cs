24fcf65b217029a8e36600e7ef3a2a6b7c56e3ab
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-01-12 22:39:42 +0530

    Update ClassifyTempBreach.cs

--- TypewiseAlert/ClassifyTempBreach.cs
+++ TypewiseAlert/ClassifyTempBreach.cs
@@ -8,9 +8,9 @@ namespace TypewiseAlert
     public static BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
     {
         List<CoolingTypeParam> listOfCoolItems = new List<CoolingTypeParam>();
-        listOfCoolItems.Add(new CoolingTypeParam() { Cooltype = CoolingType.PASSIVE_COOLING, LowerLimit = Constants.PassiveLowLimit, UpperLimit = 35 });
-        listOfCoolItems.Add(new CoolingTypeParam() { Cooltype = CoolingType.HI_ACTIVE_COOLING, LowerLimit = 0, UpperLimit = 45 });
-        listOfCoolItems.Add(new CoolingTypeParam() { Cooltype = CoolingType.MED_ACTIVE_COOLING, LowerLimit = 0, UpperLimit = 40 });
+        listOfCoolItems.Add(new CoolingTypeParam() { Cooltype = CoolingType.PASSIVE_COOLING, LowerLimit = Constants.PassiveLowLimit, UpperLimit = Constants.PassiveHighLimit });
+        listOfCoolItems.Add(new CoolingTypeParam() { Cooltype = CoolingType.HI_ACTIVE_COOLING, LowerLimit = Constants.HiActiveLowLimit, UpperLimit = Constants.HiActiveHighLimit });
+        listOfCoolItems.Add(new CoolingTypeParam() { Cooltype = CoolingType.MED_ACTIVE_COOLING, LowerLimit = Constants.MedActiveLowLimit, UpperLimit = Constants.MedActiveHighLimit });
 
         CoolingTypeParam OutputValue = listOfCoolItems.Find(m => m.Cooltype == coolingType);        
       
