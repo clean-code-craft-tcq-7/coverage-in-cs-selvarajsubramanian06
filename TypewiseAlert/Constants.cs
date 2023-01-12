07b56ecde6b796e32bb969f02941487ce519bd65
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-01-12 22:27:13 +0530

    Create Constants.cs

---
+++ TypewiseAlert/Constants.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TypewiseAlert
+{
+    public static class Constants
+    {
+      const string MailRecipient = "ab@c.com";
+      const string EmailMessage_Low = "Hi, the temperature is too low"; 
+    }
+}
